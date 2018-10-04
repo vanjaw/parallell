b97a8d27fb3c7a1747932730ef8cc8a70a456628
vanjaw <vanja.wallner@me.com>
2018-10-04 09:04:44 +0200

    Makefile added

---
+++ Assignment2/Makefile.hpp
@@ -0,0 +1,14 @@
+//
+//  Makefile.hpp
+//  MyFirstProj
+//
+//  Created by Vanjas on 2018-10-04.
+//  Copyright © 2018 Vanjas. All rights reserved.
+//
+
+#ifndef Makefile_hpp
+#define Makefile_hpp
+
+#include <stdio.h>
+
+#endif /* Makefile_hpp */
