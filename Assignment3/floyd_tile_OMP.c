fc8f11ac4cc4be2068bed04e225c9730fa224c7c
Vanja Wallner <vawa4366@siegbahn.it.uu.se>
2018-10-10 17:15:30 +0200

    IT WORKS

---
+++ Assignment3/floyd_tile_OMP.c
@@ -0,0 +1,151 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <omp.h> 
+
+/*
+ * let V= number of vertices in graph
+ * let dist = V x V array of minimum distances
+ * for each vertex v:
+ * dist[v][v] = 0
+ * for each edge (u,v):
+ * for k from 1 t oV
+ * for i from 1 to V
+ * for j from 1 to V:
+ * if dist[i][j] > dist[i][k] + dist[k][j]
+ * dist[i][j] =  dist[i][k] + dist[k][j]
+ *
+ */
+
+#define INFINITY 1000; 
+ 
+
+static int ** allocate_array(int N) {
+	int ** array;
+	int i, j, randomNr;
+	array = (int**) malloc(N * sizeof(int*));
+	for (i = 0; i < N ; i++){
+		array[i] = (int*)  malloc(N * sizeof(int));
+	}
+	for (i = 0; i < N ; i++){
+		for (j = 0; j < N ; j++){
+			if(i == j){
+				array[i][j] = 0; 
+			}
+			else{
+				randomNr = rand() % 20; //random value
+				if(randomNr > 10){
+					array[i][j] = INFINITY; 
+				}
+				else{
+					array[i][j] = randomNr;
+				}
+
+				//printf("array: i =%i, j = %i, with random number %i \n", i, j, randomNr);
+			}
+		}
+	}
+	return array;
+}
+
+static void free_array(int ** array, int N) {
+	int i;
+	for (i = 0 ; i < N ; i++){
+		free(array[i]);
+	}
+	free(array);
+}
+
+static void compute_tile(int **previous, int **current, int starti, int startj, int tilesize){
+	int t,i,j,end,oldValue,newValue;
+	end = starti + tilesize;
+	for (t = 1 ; t < end ; t++) {
+		for (i = starti ; i < end ; i++){
+			for (j = startj ; j < end; j++) {
+				oldValue = previous[i][j];
+				newValue = previous[i][t] + previous[t][j];
+				if(oldValue > newValue){
+					current[i][j] = newValue;					
+				}	
+			
+			}
+		}
+
+	}
+}
+
+ 
+int main (int argc, char * argv[]) {
+	int N;						//size of array 			
+	int ** current, **previous, **swap;				 	//array
+	int k, nrOfThreads, tilesize;	//helper variables
+
+	double time;
+	struct timeval ts,tf; 		//for timing
+	
+	/*Read input arguments*/
+	if (argc != 3) {
+		fprintf(stderr, "Usage: ./exec ArraySize NrOfThreads\n");
+		exit(-1);
+	}
+	else {
+		N = atoi(argv[1]);
+		nrOfThreads = atoi(argv[2]);
+	}
+
+	previous = allocate_array(N); //allocate array
+	current = allocate_array(N); //allocate array
+
+	gettimeofday(&ts, NULL);
+	
+	tilesize = 32;
+	int tilesPerRow = N/tilesize;
+	int nrOfTiles = tilesPerRow*tilesPerRow; 
+	int grey, whiteH, whiteV;	
+	for(k=0; k <tilesPerRow; k++){;
+		
+		{
+		// black tile
+		//printf("black tile \n");
+		compute_tile(current, previous, k*tilesize, k*tilesize, tilesize);
+
+		// grey tiles
+		#pragma omp parallel for private(grey) num_threads(nrOfThreads) 
+		for (grey = 0; grey<(tilesPerRow);  grey++){
+			if(grey != k){
+			//printf("grey tile \n");
+			compute_tile(current, previous, k*tilesize, grey*tilesize, tilesize);
+			compute_tile(current, previous, grey*tilesize, k*tilesize, tilesize);
+			}
+		}	
+		
+		// white tiles
+		#pragma omp parallel for private(whiteH, whiteV) num_threads(nrOfThreads)		
+		for (whiteH = 0; whiteH<tilesPerRow; whiteH++ ){
+			for(whiteV = 0; whiteV<tilesPerRow; whiteV++){
+				if(whiteV != k && whiteH != k){
+				//printf("white tile \n");
+				compute_tile(current, previous, whiteH*tilesize, whiteV*tilesize, tilesize);
+				}
+			}
+		}
+		}	
+	swap = previous;
+	previous = current; //both current 
+	current = swap;
+	}
+
+
+	gettimeofday(&tf, NULL);
+	time = (tf.tv_sec-ts.tv_sec)+(tf.tv_usec-ts.tv_usec)*0.000001;
+
+	free_array(current, N);	
+	free_array(previous, N);	
+	
+	printf("Floyd, array size %i, time %lf\n", N, time); 
+	return 0; 
+
+}
+
+ 
+
